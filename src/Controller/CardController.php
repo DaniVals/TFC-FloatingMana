070f3b65a1cc1b228f8fbb0ea2ddc015a5f9effd
Iván Arroyo González <ivanag2005ag@gmail.com>
2025-04-01 23:17:33 +0200

    feat: randomCard search and card search

---
+++ src/Controller/CardController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Controller;
+
+use App\Service\ScryfallApiService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CardController extends AbstractController
+{
+    private ScryfallApiService $scryfallApiService;
+
+    public function __construct(ScryfallApiService $scryfallApiService)
+    {
+        $this->scryfallApiService = $scryfallApiService;
+    }
+
+    #[Route('/carta', name: 'carta')]   
+    public function index(): Response
+    {
+        $cardInfo = $this->scryfallApiService->getRandomCard();
+        return $this->render('cardManagement/randomCard.html.twig', ['card' => $cardInfo]);
+    }
+
+    #[Route('/buscar/{nombre}', name: 'carta_buscar')]
+    public function buscar(string $nombre): Response
+    {
+        $cards = $this->scryfallApiService->searchCards($nombre);
+        return $this->render('cardManagement/searchCard.html.twig', ['cartas' => $cards]);
+    }
+}
