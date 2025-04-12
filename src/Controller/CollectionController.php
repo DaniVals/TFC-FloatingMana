786553585d7de9b09331c49f43d726879124994d
Dani Vals <daniel.vals.simon@gmail.com>
2025-04-12 11:35:18 +0200

    ADD: placeholder collection

--- src/Controller/CollectionController.php
+++ src/Controller/CollectionController.php
@@ -1,9 +1,35 @@
 <?php
 namespace App\Controller;
 
+use App\Service\ScryfallApiService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class CollectionController
+extends AbstractController
 {
     // This controller will handle the collection management
     // It will include methods to add, remove, and view cards in the collection
     // It will also include methods to manage the collection's metadata
+
+	// debug controller to check how to make a collection list
+    private ScryfallApiService $scryfallApiService;
+    public function __construct(ScryfallApiService $scryfallApiService)
+    {
+        $this->scryfallApiService = $scryfallApiService;
+    }
+
+    #[Route('/testing/vals/collection', name: 'testing_vals_collection')]
+    public function collectionRender(): Response
+    {
+		$cards = [];
+		for ($i = 0; $i < 5; $i++) {
+			$cards[] = $this->scryfallApiService->getRandomCard();
+		}
+		$user = [
+			"collection" => $cards
+		];
+        return $this->render('collectionManagement\collection.html.twig', ['user' => $user]);
+    }
 }
