174b3b623cb93b86a7b8ad9ec69781b55e927889
Dani Vals <daniel.vals.simon@gmail.com>
2025-04-13 18:38:23 +0200

    FIX: el buscador lleva a buscar con el contenido buscado

---
+++ assets/js/searchCardForm.js
@@ -0,0 +1,9 @@
+document.getElementById("search-card-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const valor = this.cardName.value.trim();
+    if (valor) {
+		const base = this.action;
+		window.location.href = base.replace("__name__", encodeURIComponent(valor));
+    }
+});
