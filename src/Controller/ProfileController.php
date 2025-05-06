223705fd12d6cb03ce1f3c2d392406aa4ddd80a0
alexm <nvr312003@gmail.com>
2025-05-06 22:17:38 +0200

    FEAT: profile

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+
+class ProfileController extends AbstractController {
+
+	#[Route('/profile', name: 'profile')]
+	public function profile()
+	{
+		$user = $this->getUser();
+		if (!$user) {
+			throw $this->createAccessDeniedException('You are not logged in.');
+		}
+
+		return $this->render('profile/profile.html.twig', [
+			'user' => $user,
+		]);
+	}
+}
