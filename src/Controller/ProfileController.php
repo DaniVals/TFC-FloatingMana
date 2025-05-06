a89f939b4e1514955c1c5f88d90317f030572b49
alexm <nvr312003@gmail.com>
2025-05-06 22:56:33 +0200

    FIX: ProfileController

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,17 +6,26 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 
 class ProfileController extends AbstractController {
-
+	
 	#[Route('/app/profile', name: 'profile')]
 	public function profile()
 	{
+		$this->denyAccessUnlessGranted('ROLE_USER');
 		$user = $this->getUser();
 		if (!$user) {
 			throw $this->createAccessDeniedException('You are not logged in.');
 		}
 
-		return $this->render('profile/profile.html.twig', [
-			'user' => $user,
-		]);
+		return $this->render('sessionManagement/profile.html.twig');
+	}
+
+	#[Route('/app/profile_update', name: 'profile_update', methods: ['POST'])]
+	public function updateProfile()
+	{
+		$this->denyAccessUnlessGranted('ROLE_USER');
+
+		// TODO hacer el update del perfil del usuario
+
+		return $this->redirectToRoute('profile');
 	}
 }
