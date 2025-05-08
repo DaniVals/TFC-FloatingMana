d1d9b2e16511bc0b2d3529e23339a6ce122a0ec1
Dani Vals <daniel.vals.simon@gmail.com>
2025-05-08 15:50:07 +0200

    FIX: input de precio de compra de pop up

--- assets/js/addCardPopUp.js
+++ assets/js/addCardPopUp.js
@@ -7,10 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
 			}
 		});
 	}
+	const colecctionCheckBox = document.getElementById('addCardPopUp-window-colecction-isFoil');
+	if (colecctionCheckBox) {
+		colecctionCheckBox.addEventListener('change', updatePrices);
+	}
 });
 
+let prices;
 
-function addCardPopUp(cardName, cardId) {
+function addCardPopUp(cardName, cardId, cardPrices) {
 	console.log("creando pop up de " + cardName + " --- " + cardId);
 
 	const popUp = document.getElementById('addCardPopUp');
@@ -21,4 +26,33 @@ function addCardPopUp(cardName, cardId) {
 
 	const cardNameDeck = document.getElementById('addCardPopUp-window-deck-name');
 	cardNameDeck.innerText = cardName;
+
+	prices = cardPrices;
+	console.log(prices);
+
+	const cardFoilColecction = document.getElementById('addCardPopUp-window-colecction-isFoil');
+	if (!prices["eur"] && prices["eur_foil"]) {
+		cardFoilColecction.checked = true;
+	}
+	
+	updatePrices();
+}
+
+function updatePrices() {
+	const cardPriceColecction = document.getElementById('addCardPopUp-window-colecction-purchacePrice');
+	const cardFoilColecction = document.getElementById('addCardPopUp-window-colecction-isFoil');
+
+	// console.log(cardFoilColecction.checked);
+	
+	if (cardPriceColecction.value == prices["eur"] || cardPriceColecction.value == prices["eur_foil"] || cardPriceColecction.value == "") {
+		if (prices["eur"] && !cardFoilColecction.checked) {
+			cardPriceColecction.value = prices["eur"];
+		}
+		else if (prices["eur_foil"] && cardFoilColecction.checked) {
+			cardPriceColecction.value = prices["eur_foil"];
+		}
+		else {
+			cardPriceColecction.value = "";	
+		}
+	}
 }
