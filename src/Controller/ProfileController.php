640bbf0e60387d4d221b432d7c6bf71087e7836b
Iván Arroyo González <ivanag2005ag@gmail.com>
2025-05-25 23:04:44 +0200

    feat: delete user function

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,8 +4,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use App\Repository\UserRepository;
 
 class ProfileController extends AbstractController {
+
+	private UserRepository $userRepository;
+
+	// Construct
+	public function __construct(UserRepository $userRepository)
+	{
+		$this->userRepository = $userRepository;
+	}
 	
 	#[Route('/app/profile', name: 'profile')]
 	public function profile()
@@ -27,5 +36,22 @@ class ProfileController extends AbstractController {
 		// TODO hacer el update del perfil del usuario
 
 		return $this->redirectToRoute('profile');
+	}
+
+	// Eliminar cuenta de usuairo
+	#[Route('/app/profile_delete', name: 'profile_delete', methods: ['POST'])]
+	public function deleteProfile()
+	{
+		$this->denyAccessUnlessGranted('ROLE_USER');
+
+		$user = $this->getUser();
+		if (!$user) {
+			throw $this->createAccessDeniedException('You are not logged in.');
+		}
+
+		// Eliminar el usuario
+		$this->userRepository->remove($user, true);
+
+		return $this->redirectToRoute('app_logout');
 	}
 }
